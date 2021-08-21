cb9f0ad2a6bedfdae5e96b49d01fca617462b63a
lsy26499 <lsy26499@gmail.com>
2021-08-21 19:23:16 +0900

    서버 cors 설정 변경

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(
   cors({
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
