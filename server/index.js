b9fbb4ab8ecec603623cb53f972dc245e64a5f1d
lsy26499 <lsy26499@gmail.com>
2021-08-21 19:49:57 +0900

    서버 cors 설정 변경

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://crud-project-test.s3-website.ap-northeast-2.amazonaws.com/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
